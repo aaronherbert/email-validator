325988797038ea5e733ed96db6a413a234bb19c9
Aaron Herbert <aaron.herbert@rac.com.au>
2024-04-08 21:16:38 +0800

    asd

--- Playwright/UnitTest1.cs
+++ Playwright/UnitTest1.cs
@@ -11,8 +11,42 @@ namespace PlaywrightTests;
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
 public class Tests : PageTest
-{  
+{
 
+    [SetUp]
+    public async Task Setup()
+    {
+
+
+        await Context.Tracing.StartAsync(new()
+        {
+            Title = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
+            Screenshots = true,
+            Snapshots = true,
+            Sources = true
+        });
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        var fileName = $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip";
+        foreach (var c in Path.GetInvalidFileNameChars())
+        {
+            fileName = fileName.Replace(c, '-');
+        }
+
+        // This will produce e.g.:
+        // bin/Debug/net8.0/playwright-traces/PlaywrightTests.Tests.Test1.zip
+        await Context.Tracing.StopAsync(new()
+        {
+            Path = Path.Combine(
+                TestContext.CurrentContext.WorkDirectory,
+                "playwright-traces",
+                fileName
+            )
+        });
+    }
 
     [TestCase("test@test.com")]
     public async Task good(string email)
@@ -34,6 +68,7 @@ public class Tests : PageTest
 
     [TestCase("asd")]
     [TestCase("123")]
+    [TestCase("test@test.com")]
     public async Task bad(string email)
     {
         await Page.GotoAsync("https://www.bankwest.com.au/retail-forms/customer-care");
