63984bf1b818b17d0a47a35955c4cac15cb5d5f6
Aaron Herbert <aaron.herbert@rac.com.au>
2024-04-09 22:15:48 +0800

    asd

--- Playground/PlaygroundTests/EmailValidatorTests.cs
+++ Playground/PlaygroundTests/EmailValidatorTests.cs
@@ -86,7 +86,32 @@ namespace PlaygroundTests
         [InlineData("@example.com")]
         [InlineData("phrase: abigail@example.com abigail@example.com ;")]
         [InlineData("invalid�char@example.com")]
-    public void IsPrime_InputIs1_ReturnFalse(string email)
+
+
+        [InlineData("emmanuel.hibernate.org")]
+        [InlineData("emma nuel@hibernate.org")]
+        [InlineData("emma(nuel@hibernate.org")]
+        [InlineData("emmanuel@")]
+        [InlineData("emma\nnuel@hibernate.org")]
+        [InlineData("emma@nuel@hibernate.org")]
+        [InlineData("emma@nuel@.hibernate.org")]
+        [InlineData("Just a string")]
+ 
+        [InlineData("me@")]
+ 
+        [InlineData("me.@example.com")]
+        [InlineData(".me@example.com")]
+        [InlineData("me@example..com")]
+        [InlineData("me\\@example.com")]
+        [InlineData("Abc.example.com")] // (no @ character)
+        [InlineData("A@b@c@example.com")] // (only one @ is allowed outside quotation marks)
+        [InlineData("a\"b(c)d,e:f;g<h>i[j\\k]l@example.com")] // (none of the special characters in this local-part are allowed outside quotation marks)
+        [InlineData("just\"not\"right@example.com")] // (quoted strings must be dot separated or the only element making up the local-part)
+        [InlineData("this is\"not\\allowed@example.com")] // (spaces, quotes, and backslashes may only exist when within quoted strings and preceded by a backslash)
+        [InlineData("this\\ still\\\"not\\\\allowed@example.com")] // (even if escaped (preceded by a backslash), spaces, quotes, and backslashes must still be contained by quotes)
+        [InlineData("john..doe@example.com")] // (double dot before @) with caveat: Gmail lets this through, Email address#Local-part the dots altogether
+        [InlineData("john.doe@example..com")]
+        public void IsPrime_InputIs1_ReturnFalse(string email)
     {
         EmailValidator.Validate(email).ShouldBeFalse();
     }
@@ -109,48 +134,42 @@ namespace PlaygroundTests
     [InlineData("\"joe*\"@apache.org")]
     [InlineData("szffakylcxnsqzhsesacplocmhnkslqvjwjpxqeohyxkvicqhgzpuisglcttwntq@hbf.com.au")]
     [InlineData("test@Bücher.ch")]
-    [InlineData("abigail@example.com")]
-    [InlineData("abigail@example.com ")]
-    [InlineData(" abigail@example.com")]
-    [InlineData("abigail @example.com ")]
-    [InlineData("*@example.net")]
-    [InlineData("\"\\\"\"@foo.bar")]
-    [InlineData("fred&barny@example.com")]
-    [InlineData("---@example.com")]
-    [InlineData("foo-bar@example.net")]
-    [InlineData("\"127.0.0.1\"@[127.0.0.1]")]
-    [InlineData("Abigail <abigail@example.com>")]
-    [InlineData("Abigail<abigail@example.com>")]
-    [InlineData("Abigail<@a,@b,@c:abigail@example.com>")]
-    [InlineData("\"This is a phrase\"<abigail@example.com>")]
-    [InlineData("\"Abigail \"<abigail@example.com>")]
-    [InlineData("\"Joe & J. Harvey\" <example @Org>")]
-    [InlineData("Abigail <abigail @ example.com>")]
-    [InlineData("Abigail made this <  abigail   @   example  .    com    >")]
-    [InlineData("Abigail(the bitch)@example.com")]
-    [InlineData("Abigail <abigail @ example . (bar) com >")]
-    [InlineData("Abigail < (one)  abigail (two) @(three)example . (bar) com (quz) >")]
-    [InlineData("Abigail (foo) (((baz)(nested) (comment)) ! ) < (one)  abigail (two) @(three)example . (bar) com (quz) >")]
-    [InlineData("Abigail <abigail(fo\\(o)@example.com>")]
-    [InlineData("Abigail <abigail(fo\\)o)@example.com> ")]
-    [InlineData("(foo) abigail@example.com")]
-    [InlineData("abigail@example.com (foo)")]
-    [InlineData("\"Abi\\\"gail\" <abigail@example.com>")]
-    [InlineData("abigail@[example.com]")]
-    [InlineData("abigail@[exa\\[ple.com]")]
-    [InlineData("abigail@[exa\\]ple.com]")]
-    [InlineData("\":sysmail\"@  Some-Group. Some-Org")]
-    
+        [InlineData( "emmanuel@hibernate.org")]
+        [InlineData( "emmanuel@hibernate")]
+        [InlineData( "emma-n_uel@hibernate")]
+        [InlineData( "emma+nuel@hibernate.org")]
+        [InlineData( "emma=nuel@hibernate.org")]
+        [InlineData( "emmanuel@[123.12.2.11]")]
+        [InlineData( "*@example.net")]
+        [InlineData( "fred&barny@example.com")]
+        [InlineData( "---@example.com")]
+        [InlineData( "foo-bar@example.net")]
+        [InlineData( "mailbox.sub1.sub2@this-domain")]
+        [InlineData( "prettyandsimple@example.com")]
+        [InlineData( "very.common@example.com")]
+        [InlineData( "disposable.style.email.with+symbol@example.com")]
+        [InlineData( "other.email-with-dash@example.com")]
+        [InlineData( "x@example.com")]
+        [InlineData( "\"much.more unusual\"@example.com")]
+        [InlineData( "\"very.unusual.@.unusual.com\"@example.com")]
+        [InlineData( "\"very.(),:;<>[]\\\".VERY.\\\"very@\\\\ \\\"very\\\".unusual\"@strange.example.com")]
+        [InlineData( "\"some \".\" strange \".\" part*:; \"@strange.example.com")]
+        [InlineData( "example-indeed@strange-example.com")]
+        [InlineData( "admin@mailserver1")]
+        [InlineData( "#!$%&'*+-/=?^_`{}|~@example.org")]
+        [InlineData( "\"()<>[]:,;@\\\"!#$%&'-/=?^_`{}| ~.a\"@example.org")]
+        [InlineData( "\" \"@example.org")]
+        [InlineData( "example@localhost")]
+        [InlineData( "example@s.solutions")]
+        [InlineData( "user@localserver")]
+        [InlineData( "user@tt")]
+        [InlineData( "user@[IPv6:2001:DB8::1]")]
+        [InlineData( "xn--80ahgue5b@xn--p-8sbkgc5ag7bhce.xn--ba-lmcq")]
+        [InlineData( "nothing@xn--fken-gra.no")]
 
-
-    public void UpdateMemberEmailAddressCommand_Email_Valid(string email)
+        public void UpdateMemberEmailAddressCommand_Email_Valid(string email)
     {
         EmailValidator.Validate(email).ShouldBeTrue();
-
-
-
-
-
     }
 }
 }
