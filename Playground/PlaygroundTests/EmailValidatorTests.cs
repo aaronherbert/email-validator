572a1d5f979d9f3862dcb2dd93ab06aa8b4b00d6
Aaron Herbert <aaron.herbert@rac.com.au>
2024-04-09 20:58:11 +0800

    asd

--- Playground/PlaygroundTests/EmailValidatorTests.cs
+++ Playground/PlaygroundTests/EmailValidatorTests.cs
@@ -69,33 +69,88 @@ namespace PlaygroundTests
         [InlineData("john56789.john56789.john56789.john56789.john56789.john56789.john5@example.com")]
         [InlineData("Abc@def @example.com")]
         [InlineData("abc @abc_def.com")]
-        public void IsPrime_InputIs1_ReturnFalse(string email)
-        {
-            EmailValidator.Validate(email).ShouldBeFalse();
-        }
-
-        [Theory]
-        [InlineData("test@hbf.com.au")]
-        [InlineData("test@blah.museum")]
-
-        [InlineData("js@proseware.com9")] // this seems important
-        [InlineData("j.s@server1.proseware.com")]
-        [InlineData("js@contoso.中国")]
-        [InlineData("j@proseware.com9")]
-        [InlineData("js#internal@proseware.com")]
-        [InlineData("j_9@[129.126.118.1]")]
-        [InlineData("david.jones@proseware.com")]
-        [InlineData("d.j@server1.proseware.com")]
-        [InlineData("jones@ms1.proseware.com")]
-        [InlineData("test@1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.com")]
-        //[InlineData("test@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg.ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg.ggggggggggggggggggggggggggggggggggggggggggggggggggggggggg.com")]
-        [InlineData("\"joe*\"@apache.org")]
-        [InlineData("szffakylcxnsqzhsesacplocmhnkslqvjwjpxqeohyxkvicqhgzpuisglcttwntq@hbf.com.au")]
-        [InlineData("test@Bücher.ch")]
-        
-        public void UpdateMemberEmailAddressCommand_Email_Valid(string email)
-        {
-            EmailValidator.Validate(email).ShouldBeTrue();
-        }
+        [InlineData("   Just a string")]
+        [InlineData("string")]
+        [InlineData("(comment)")]
+        [InlineData("()@example.com")]
+        [InlineData("fred(&)barny@example.com")]
+        [InlineData("fred\\ barny@example.com")]
+        [InlineData("Abigail <abi gail @ example.com>")]
+        [InlineData("Abigail <abigail(fo(o)@example.com>")]
+        [InlineData("Abigail <abigail(fo)o)@example.com>")]
+        [InlineData("\"Abi\"gail\" <abigail@example.com>")]
+        [InlineData("abigail@[exa]ple.com]")]
+        [InlineData("abigail@[exa[ple.com]")]
+        [InlineData("abigail@[exaple].com]")]
+        [InlineData("abigail@")]
+        [InlineData("@example.com")]
+        [InlineData("phrase: abigail@example.com abigail@example.com ;")]
+        [InlineData("invalid�char@example.com")]
+    public void IsPrime_InputIs1_ReturnFalse(string email)
+    {
+        EmailValidator.Validate(email).ShouldBeFalse();
     }
+
+    [Theory]
+    [InlineData("test@hbf.com.au")]
+    [InlineData("test@blah.museum")]
+
+    [InlineData("js@proseware.com9")] // this seems important
+    [InlineData("j.s@server1.proseware.com")]
+    [InlineData("js@contoso.中国")]
+    [InlineData("j@proseware.com9")]
+    [InlineData("js#internal@proseware.com")]
+    [InlineData("j_9@[129.126.118.1]")]
+    [InlineData("david.jones@proseware.com")]
+    [InlineData("d.j@server1.proseware.com")]
+    [InlineData("jones@ms1.proseware.com")]
+    [InlineData("test@1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.1234567890.com")]
+    //[InlineData("test@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg.ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg.ggggggggggggggggggggggggggggggggggggggggggggggggggggggggg.com")]
+    [InlineData("\"joe*\"@apache.org")]
+    [InlineData("szffakylcxnsqzhsesacplocmhnkslqvjwjpxqeohyxkvicqhgzpuisglcttwntq@hbf.com.au")]
+    [InlineData("test@Bücher.ch")]
+    [InlineData("abigail@example.com")]
+    [InlineData("abigail@example.com ")]
+    [InlineData(" abigail@example.com")]
+    [InlineData("abigail @example.com ")]
+    [InlineData("*@example.net")]
+    [InlineData("\"\\\"\"@foo.bar")]
+    [InlineData("fred&barny@example.com")]
+    [InlineData("---@example.com")]
+    [InlineData("foo-bar@example.net")]
+    [InlineData("\"127.0.0.1\"@[127.0.0.1]")]
+    [InlineData("Abigail <abigail@example.com>")]
+    [InlineData("Abigail<abigail@example.com>")]
+    [InlineData("Abigail<@a,@b,@c:abigail@example.com>")]
+    [InlineData("\"This is a phrase\"<abigail@example.com>")]
+    [InlineData("\"Abigail \"<abigail@example.com>")]
+    [InlineData("\"Joe & J. Harvey\" <example @Org>")]
+    [InlineData("Abigail <abigail @ example.com>")]
+    [InlineData("Abigail made this <  abigail   @   example  .    com    >")]
+    [InlineData("Abigail(the bitch)@example.com")]
+    [InlineData("Abigail <abigail @ example . (bar) com >")]
+    [InlineData("Abigail < (one)  abigail (two) @(three)example . (bar) com (quz) >")]
+    [InlineData("Abigail (foo) (((baz)(nested) (comment)) ! ) < (one)  abigail (two) @(three)example . (bar) com (quz) >")]
+    [InlineData("Abigail <abigail(fo\\(o)@example.com>")]
+    [InlineData("Abigail <abigail(fo\\)o)@example.com> ")]
+    [InlineData("(foo) abigail@example.com")]
+    [InlineData("abigail@example.com (foo)")]
+    [InlineData("\"Abi\\\"gail\" <abigail@example.com>")]
+    [InlineData("abigail@[example.com]")]
+    [InlineData("abigail@[exa\\[ple.com]")]
+    [InlineData("abigail@[exa\\]ple.com]")]
+    [InlineData("\":sysmail\"@  Some-Group. Some-Org")]
+    
+
+
+    public void UpdateMemberEmailAddressCommand_Email_Valid(string email)
+    {
+        EmailValidator.Validate(email).ShouldBeTrue();
+
+
+
+
+
+    }
+}
 }
